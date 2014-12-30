33934d33b349116694f68428550a82a066b490ab
Barrie Treloar <baerrach@gmail.com>
2014-12-30 16:45:29 +1030

    Rewrite it tests to use Meteor.login and check roles

--- tests/jasmine/client/integration/headerTemplateSpec.js
+++ tests/jasmine/client/integration/headerTemplateSpec.js
@@ -1,21 +1,50 @@
 "use strict";
 
-describe("Header template", function () {
-    it("should not show create light novel link to non-admins", function () {
+describe("Header template (anonymous users)", function () {
+
+    it("should not show create light novel link", function () {
         var div = document.createElement("DIV");
+        
         Blaze.render(Template.header, div);
- 
+        
         expect($(div).find("#createLightNovel")[0]).not.toBeDefined();
     });
+});
 
-    it("should show create light novel link to admins", function () {
+describe("Header template (non-admin role)", function () {
+    
+    beforeEach(function (done) {
+        Meteor.loginWithPassword('normal@example.com', 'normal42', done);
+    });
+    
+    afterEach(function (done) {
+        Meteor.logout(done);
+    });
+
+    it("should not show create light novel link", function () {
         var div = document.createElement("DIV");
-
-        spyOn(Blaze._globalHelpers, "isInRole").and.returnValue(true);
 
         Blaze.render(Template.header, div);
 
-        expect(Blaze._globalHelpers.isInRole).toHaveBeenCalledWith("admin", jasmine.any(Object));
+        expect($(div).find("#createLightNovel")[0]).not.toBeDefined();
+    });
+});
+
+describe("Header template (admin role)", function () {
+    
+    beforeEach(function (done) {
+        Meteor.loginWithPassword('admin@example.com', 'admin42', done);
+    });
+    
+    afterEach(function (done) {
+        Meteor.logout(done);
+    });
+
+    it("should show create light novel link", function () {
+        var div = document.createElement("DIV");
+
+        Blaze.render(Template.header, div);
+
         expect($(div).find("#createLightNovel")[0]).toBeDefined();
     });
 
